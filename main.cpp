b9eedb9ef6e459582baa778d0d91443445200b6e
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-26 00:31:37 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//Hoppock, Zachary
+//Chapter 1.12 RE
+//8/30/2020
+//Writing an algorith to solve a word problem
+// Version  #1
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  cout << "start * 7 = " << 6 * 7 << endl;
+
+  return 0;
+
+}
